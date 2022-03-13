db9e9e63640a1a5cc6df158e43b97d6f240a4ecf
YouHyerin <you020206@gnu.ac.kr>
2022-03-14 05:49:35 +0900

    added MenuManagement.java

---
+++ src/MenuManagement.java
@@ -0,0 +1,13 @@
+
+public class MenuManagement {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Constellation");
+		System.out.println("2. Delet Constellation");
+		System.out.println("3. Edit Constellation");
+		System.out.println("4. View Constellation");
+		System.out.println("5. Exit");
+
+	}
+
+}

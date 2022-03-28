f57d9552544c0d1aea0a0eaac0c174b4fd0106c9
YouHyerin <you020206@gnu.ac.kr>
2022-03-28 19:02:09 +0900

    added MenuManagementSystem

--- src/MenuManagement.java
+++ src/MenuManagement.java
@@ -15,24 +15,57 @@ public class MenuManagement {
 			System.out.println("5. Exit");
 			System.out.println("Select one number between 1-6: ");
 			num = input.nextInt();
-			switch (num) {
-			case 1:
-				System.out.print("Constellation Name: ");
-				String ConstellationName = input.next();
-				System.out.print("Constellation Main Star: ");
-				String ConstellationMainStar = input.next();
-				System.out.print("Observable Season(northern hemisphere standard): ");
-				String ObservableSeason = input.next();
-				break;
-			case 2:
-			case 3:
-			case 4:
-				System.out.print("Constellation Name: ");
-				String ConstellationName2 = input.next();			
+			if (num == 1) {
+				addConstellation();
 			}
-			
+			else if (num == 2) {
+				deletConstellation();
+			}
+			else if (num == 3) {
+				editConstellation();
+			}
+			else if (num == 4) {
+				viewConstellation();
+			}
+			else {
+				continue;
+			}
 		}
 
 	}
 
+	public static void addConstellation() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Constellation Name: ");
+		String ConstellationName = input.nextLine();
+		System.out.println(ConstellationName);
+		System.out.print("Constellations Main Star: ");
+		String ConstellationMainStar = input.nextLine();
+		System.out.println(ConstellationMainStar);
+		System.out.print("Observable Season(northern hemisphere standard): ");
+		String ObservableSeason = input.next();
+		System.out.println(ObservableSeason);
+		
+	}
+	public static void deletConstellation() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the Delet Constellation Name: ");
+		String DeletConstellationName = input.nextLine();
+		System.out.println("Delet the " + DeletConstellationName + "?");
+		System.out.print("Yes or No: ");
+		String YON = input.next();
+	}
+	public static void editConstellation() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the Edit Constellation Name: ");
+		String EditConstellationName = input.nextLine();
+		System.out.println(EditConstellationName);
+		
+	}
+	public static void viewConstellation() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the View Constellation Name: ");
+		String ViewConstellationName = input.nextLine();
+		System.out.println(ViewConstellationName);
+	}
 }

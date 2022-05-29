eaa9c6476369d80ec8030bd176d38db4de528b25
YouHyerin <you020206@gnu.ac.kr>
2022-05-30 08:04:21 +0900

    added GuiTest

--- src/gui/GuiTest.java
+++ src/gui/GuiTest.java
@@ -3,7 +3,7 @@ package gui;
 public class GuiTest {
 
 	public static void main(String[] args) {
-		//MenuSelection menuselection = new MenuSelection();
+		MenuSelection menuselection = new MenuSelection();
 		ConstellationAdder constellationadder = new ConstellationAdder();
 		ConstellationViewer constellationviewer = new ConstellationViewer();
 	}

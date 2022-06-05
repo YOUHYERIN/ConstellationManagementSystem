bc44e532f6a43ae7e054fc11c1c5fc4a1e60a521
YouHyerin <you020206@gnu.ac.kr>
2022-06-06 00:12:10 +0900

    added GuiTest and added MenuSelection

--- src/gui/MenuSelection.java
+++ src/gui/MenuSelection.java
@@ -3,25 +3,33 @@ package gui;
 import java.awt.BorderLayout;
 
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class MenuSelection extends JFrame {
+import listener.ButtonAddListener;
+import listener.ButtonViewListener;
 
-	public MenuSelection() {
-		this.setSize(400, 400);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+public class MenuSelection extends JPanel {
+	
+	WindowFrame frame;
+
+	public MenuSelection(WindowFrame frame) {
+		this.frame = frame;
+		
+		this.setLayout(new BorderLayout());
 		
 		JPanel panel1 = new JPanel();
 		JPanel panel2 = new JPanel();
 		JLabel label = new JLabel("Menu Selection");
 		
 		JButton button1 = new JButton("Add Constellation");
-		JButton button2 = new JButton("Delete Constellation");
+		JButton button2 = new JButton("Delet Constellation");
 		JButton button3 = new JButton("Edit Constellation");
 		JButton button4 = new JButton("View Constellation");
 		JButton button5 = new JButton("Exit Program");
+		
+		button1.addActionListener(new ButtonAddListener(frame));
+		button4.addActionListener(new ButtonViewListener(frame));
 		
 		panel1.add(label);
 		panel2.add(button1);
@@ -31,8 +39,6 @@ public class MenuSelection extends JFrame {
 		panel2.add(button5);
 		
 		this.add(panel1, BorderLayout.NORTH);
-		this.add(panel2, BorderLayout.CENTER);
-		this.setVisible(true);
-		
+		this.add(panel2, BorderLayout.CENTER);		
 	}
 }

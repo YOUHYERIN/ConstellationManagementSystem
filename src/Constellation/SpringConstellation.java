70562df24fd15b3f64e60e3912f6704082cf8c41
YouHyerin <you020206@gnu.ac.kr>
2022-06-13 19:47:22 +0900

    added SpringConstellation

--- src/Constellation/SpringConstellation.java
+++ src/Constellation/SpringConstellation.java
@@ -14,7 +14,6 @@ public class SpringConstellation extends Constellation {
 		setConstellationNumber(input);
 		setConstellationName(input);
 		setConstellationMainStar(input);
-
 	}
 	
 	public void printInfo() {
